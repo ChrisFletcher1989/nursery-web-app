c374cb7b3fbb53388f938a973eb4baa17ba99926
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-10-11 12:42:27 +0900

    update-weather

--- weather.js
+++ weather.js
@@ -23,3 +23,27 @@ window.addEventListener('load', function () {
         console.error('Error fetching data:', error);
     });
 });
+window.addEventListener('load', function () {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&hourly=temperature_2m,weathercode&current_weather=true&forecast_days=1')
+        .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+        .then(data => {
+        console.log(data.current_weather.temperature);
+        const currentTemperature = data.current_weather.temperature;
+        const weatherList = document.getElementById('weatherList');
+        if (weatherList !== null) {
+            const listItem = document.createElement('li');
+            listItem.textContent = `ロンドン: ${currentTemperature}°C`;
+            weatherList.appendChild(listItem);
+            listItem.classList.add('home');
+            console.log(currentTemperature);
+        }
+    })
+        .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+});
--- weather.ts
+++ weather.ts
@@ -24,3 +24,30 @@ window.addEventListener('load', function () {
             console.error('Error fetching data:', error);
         });
 });
+
+window.addEventListener('load', function () {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=51.51&longitude=-0.13&hourly=temperature_2m,weathercode&current_weather=true&forecast_days=1')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.current_weather.temperature); 
+
+            const currentTemperature:number = data.current_weather.temperature; 
+
+            const weatherList:HTMLElement | null = document.getElementById('weatherList');
+            if (weatherList !== null) {
+                const listItem:HTMLElement = document.createElement('li');
+                listItem.textContent = `ロンドン: ${currentTemperature}°C`;
+                weatherList.appendChild(listItem);
+                listItem.classList.add('home');
+                console.log(currentTemperature);
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
