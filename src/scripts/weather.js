fd86dd62ce574a59ba1fe0d82580ed8d863181da
ChrisFletcher1989 <chrisfletcher1989@gmail.com>
2023-11-14 09:55:54 +0900

    edits-to-homeP-text

--- src/scripts/weather.js
+++ src/scripts/weather.js
@@ -43,17 +43,75 @@ window.addEventListener('load', function () {
         console.error('Error fetching data:', error);
     });
 });
+//////////////BOXES ON CLICK/////////////////////
 const getEngStyle = document.getElementById('engStyle');
+const getEngBox = document.getElementById('engBox');
+const getElegantBox = document.getElementById('elegantBox');
+const getElegantText = document.getElementById('elegantText');
+const getPeaceBox = document.getElementById('peaceBox');
+const getPeaceText = document.getElementById('peaceText');
+const getInOutBox = document.getElementById('inOutBox');
+const getInOutText = document.getElementById('inOutText');
+const getSecurityBox = document.getElementById('securityBox');
+const getSecurityText = document.getElementById('securityText');
 let isToggled = false;
 const changeEngStyle = () => {
-    if (getEngStyle) {
-        if (isToggled) {
-            getEngStyle.innerHTML = "more info";
+    if (getEngBox) {
+        if (isToggled && getEngStyle != null) {
+            getEngStyle.innerHTML = "More info here";
         }
-        else {
+        else if (getEngStyle != null) {
             getEngStyle.innerHTML = "当園のカリキュラムは、免許を持つ保育士である梅田美貴子と経験豊富なイギリス人教師クリス・フレッチャーによって作成されました。英国とスコットランドの専門家パネルの指導を受け、ブリティッシュスタイルの学びと日本の質をバランスよく取り入れたカリキュラムです.";
         }
         isToggled = !isToggled;
     }
 };
-getEngStyle === null || getEngStyle === void 0 ? void 0 : getEngStyle.addEventListener("click", changeEngStyle);
+const changeElegantText = () => {
+    if (getElegantBox) {
+        if (isToggled && getElegantText != null) {
+            getElegantText.innerHTML = "More info here";
+        }
+        else if (getElegantText != null) {
+            getElegantText.innerHTML = "Elegant & Politeここでは、お子さま同士が礼儀正しく接することや、良いマナーを身につけること、上品な日本語で話すことを奨励しています。それぞれが 'The King's English' と上品な日本語を組み合わせた、丁寧で気品あるコミュニケーションを目指しています.";
+        }
+        isToggled = !isToggled;
+    }
+};
+const changePeaceText = () => {
+    if (getPeaceBox) {
+        if (isToggled && getPeaceText != null) {
+            getPeaceText.innerHTML = "More info here";
+        }
+        else if (getPeaceText != null) {
+            getPeaceText.innerHTML = "DECIDE";
+        }
+        isToggled = !isToggled;
+    }
+};
+const changeInOutText = () => {
+    if (getInOutBox) {
+        if (isToggled && getInOutText != null) {
+            getInOutText.innerHTML = "More info here";
+        }
+        else if (getInOutText != null) {
+            getInOutText.innerHTML = "DECIDE";
+        }
+        isToggled = !isToggled;
+    }
+};
+const changeSecurityText = () => {
+    if (getSecurityBox) {
+        if (isToggled && getSecurityText != null) {
+            getSecurityText.innerHTML = "More info here";
+        }
+        else if (getSecurityText != null) {
+            getSecurityText.innerHTML = "DECIDE";
+        }
+        isToggled = !isToggled;
+    }
+};
+getEngBox === null || getEngBox === void 0 ? void 0 : getEngBox.addEventListener("click", changeEngStyle);
+getElegantBox === null || getElegantBox === void 0 ? void 0 : getElegantBox.addEventListener("click", changeElegantText);
+getPeaceBox === null || getPeaceBox === void 0 ? void 0 : getPeaceBox.addEventListener("click", changePeaceText);
+getInOutBox === null || getInOutBox === void 0 ? void 0 : getInOutBox.addEventListener("click", changeInOutText);
+getSecurityBox === null || getSecurityBox === void 0 ? void 0 : getSecurityBox.addEventListener("click", changeSecurityText);
--- src/scripts/weather.ts
+++ src/scripts/weather.ts
@@ -47,20 +47,78 @@ window.addEventListener('load', function () {
             console.error('Error fetching data:', error);
         });
 });
-
+//////////////BOXES ON CLICK/////////////////////
 const getEngStyle = document.getElementById('engStyle') as HTMLImageElement | null;
+const getEngBox = document.getElementById('engBox') as HTMLImageElement | null;
+const getElegantBox = document.getElementById('elegantBox') as HTMLImageElement | null;
+const getElegantText = document.getElementById('elegantText') as HTMLImageElement | null;
+const getPeaceBox = document.getElementById('peaceBox') as HTMLImageElement | null;
+const getPeaceText = document.getElementById('peaceText') as HTMLImageElement | null;
+const getInOutBox = document.getElementById('inOutBox') as HTMLImageElement | null;
+const getInOutText = document.getElementById('inOutText') as HTMLImageElement | null;
+const getSecurityBox = document.getElementById('securityBox') as HTMLImageElement | null;
+const getSecurityText = document.getElementById('securityText') as HTMLImageElement | null;
 
 let isToggled = false;
 
 const changeEngStyle = () => {
-    if (getEngStyle) {
-        if (isToggled) {
+    if (getEngBox) {
+        if (isToggled && getEngStyle!=null) {
             getEngStyle.innerHTML = "More info here" 
-        } else {
+        } else if (getEngStyle!=null){
             getEngStyle.innerHTML = "当園のカリキュラムは、免許を持つ保育士である梅田美貴子と経験豊富なイギリス人教師クリス・フレッチャーによって作成されました。英国とスコットランドの専門家パネルの指導を受け、ブリティッシュスタイルの学びと日本の質をバランスよく取り入れたカリキュラムです.";
         }
         isToggled = !isToggled;
     }
 }
-getEngStyle?.addEventListener("click", changeEngStyle);
+
+const changeElegantText = () => {
+    if (getElegantBox) {
+        if (isToggled && getElegantText!=null) {
+            getElegantText.innerHTML = "More info here" 
+        } else if (getElegantText!=null){
+            getElegantText.innerHTML = "Elegant & Politeここでは、お子さま同士が礼儀正しく接することや、良いマナーを身につけること、上品な日本語で話すことを奨励しています。それぞれが 'The King's English' と上品な日本語を組み合わせた、丁寧で気品あるコミュニケーションを目指しています.";
+        }
+        isToggled = !isToggled;
+    }
+}
+const changePeaceText = () => {
+    if (getPeaceBox) {
+        if (isToggled && getPeaceText!=null) {
+            getPeaceText.innerHTML = "More info here" 
+        } else if (getPeaceText!=null){
+            getPeaceText.innerHTML = "DECIDE";
+        }
+        isToggled = !isToggled;
+    }
+}
+const changeInOutText = () => {
+    if (getInOutBox) {
+        if (isToggled && getInOutText!=null) {
+            getInOutText.innerHTML = "More info here" 
+        } else if (getInOutText!=null){
+            getInOutText.innerHTML = "DECIDE";
+        }
+        isToggled = !isToggled;
+    }
+}
+const changeSecurityText = () => {
+    if (getSecurityBox) {
+        if (isToggled && getSecurityText!=null) {
+            getSecurityText.innerHTML = "More info here" 
+        } else if (getSecurityText!=null){
+            getSecurityText.innerHTML = "DECIDE";
+        }
+        isToggled = !isToggled;
+    }
+}
+getEngBox?.addEventListener("click", changeEngStyle);
+getElegantBox?.addEventListener("click", changeElegantText);
+getPeaceBox?.addEventListener("click", changePeaceText);
+getInOutBox?.addEventListener("click", changeInOutText);
+getSecurityBox?.addEventListener("click", changeSecurityText);
+
+
+
+
 
